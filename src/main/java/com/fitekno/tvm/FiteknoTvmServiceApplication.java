6f31ac978eb315160425c9611fd9d3df012d2cab
Abdurreh Dagdelen <abdurrehim.dagdelen@id3.com.tr>
2023-03-29 01:06:37 +0300

    Initial commit

---
+++ src/main/java/com/fitekno/tvm/FiteknoTvmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fitekno.tvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiteknoTvmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiteknoTvmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fitekno/tvm/FiteknoTvmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitekno.tvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiteknoTvmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
